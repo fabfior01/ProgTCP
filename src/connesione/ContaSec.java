0e80346159f187cff06b08cdc5bd1d2282db060b
Fabio Fiorucci <fabiofiorucci6@gmail.com>
2020-04-03 11:57:22 +0200

    Modifica Struttura

--- src/connesione/ContaSec.java
+++ src/connesione/ContaSec.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package progettoStrikeBall;
+package connesione;
 
 import static java.lang.Thread.sleep;
 
